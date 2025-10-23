35bf8284e492cc15f1f422d636c7465c44ff7fb6
jeet <jeetjadav11@gmail.com>
2025-10-23 14:15:25 -0400

    did 33

--- Automaton.java
+++ Automaton.java
@@ -46,23 +46,15 @@ public class Automaton
      */
     public void update()
     {
-        // Build the new state in a separate array.
-        //int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        int oldLeft = 0;
-        for(int i = 0; i < state.length; i++) {
-            
-            int left =(i == 0)? 0 : oldLeft;        
-            
-            int center = state[i];
-            int right = (i + 1 < state.length)? state[i + 1]:0;
-            
-            oldLeft = state[i];
-            state[i] = (left + center + right) % 2;
-            
-        }
-        //state = nextState;
+        int[] nextState = new int [state.length];
+        int left = 0;
+        int center = state[0];
+        for (int i=0; i<state.length; i++){
+        int right = i + 1 < state.length ? state[i+1] : 0;
+        nextState[i] = (left + center + right) % 2;
+        left = center;
+        center = right;
+      }
     }
     
     /**
